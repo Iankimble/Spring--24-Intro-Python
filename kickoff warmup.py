563f27635c823ed6b81c29f8c6870cf893325103
Ian <38480036+Iankimble@users.noreply.github.com>
2024-02-02 15:07:53 +0000

    activity review

--- kickoff warmup.py
+++
@@ -1,8 +0,0 @@
-answer= input('did you sumbit your code? ')
-if answer== 'y':
-    print('good job, have a great day.') 
-else:
-    print("submit work before you go. ")
-
-integer= 100
-float= 100.00
