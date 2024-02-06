4f73cc4f772e27229d1abdc3f9d3b79fb073c8e5
Ian <38480036+Iankimble@users.noreply.github.com>
2024-02-06 15:22:05 +0000

    activity for feb 2

---
+++ avtivityFeb6.py
@@ -0,0 +1,15 @@
+# Q: What is complexity ?
+# A: The measure of how resource 
+# intensive a program can be.
+
+# Q: What is abstraction ?
+# A: Abstraction is the process of
+# simplifying or hiding complexity.
+
+
+print('hello ')
+print(200)
+
+dataFromUser= input("what is your name? ") # puts in data
+
+print(dataFromUser)
