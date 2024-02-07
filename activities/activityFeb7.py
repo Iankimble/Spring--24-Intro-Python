2bc6ba103e44d43b54445f1a006e19bfe1e3cab7
Ian <38480036+Iankimble@users.noreply.github.com>
2024-02-07 16:22:45 +0000

    Structuring github repo

---
+++ activities/activityFeb7.py
@@ -0,0 +1,40 @@
+# Daily Warmup
+
+# Create a new file called activityFeb7.py
+# and answer the following questions
+
+# 1. In your own words, what is a computer program?
+
+# 2. Name three (3) data types
+
+# 3. Name three (3) python Operators
+
+# READING PROMPTS
+# Carefully read through each scenario and determine which Python Operator you would use to build the
+# particular program. 
+
+# Read the question carfully
+# Find key words that you recognized to deduce what it could be
+# use context clues provided in the prompt ot deduce what it could be.
+
+# 4. You have been hired as a software engineer to develop a gym membership system.
+# Your program needs to verify two (2) things; if the user is a member of the gym, AND
+# if the user has the gold membership tier, which allows the user to have access to the swimming pool.
+# What your client would like to happen is that when a user is a member AND 
+# and also has the gold membership they should be able to use the pool.
+# Which python operator would you use in this situation? Explain why
+
+# 5. You have been hired as a software engineer to develop a simple payment system for a companys 
+# webstore. Your assignment is to create a cart system where the prices of all 
+# the items are added together and then deducts a 20% discount at checkout. 
+# Which python operator would you use in this situation? Explain why.
+
+# 6. You have been hired as an engineer to develop a student badge system. Your client would like to 
+# have a system where when a student swipes their badge, the ID number on the badge is compared to the ID
+# number in the database.If the ID numbers match and are the same the student can come in, if it does not
+# match, they cannot enter.
+# Which python operator would you use in this situation? Explain why
+ 
+
+ 
+
