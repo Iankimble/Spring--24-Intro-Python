ef1583ba65f4564bd3ac91c3c0f76137dbd2f36e
Ian <38480036+Iankimble@users.noreply.github.com>
2024-02-09 17:58:20 +0000

    daily actvity for the day.

---
+++ lesson.py
@@ -0,0 +1,23 @@
+# input()
+# print()
+
+# Data casting functions --> changes the values data type.
+testNumber=int(40.9899999)
+print(testNumber)
+
+str()
+
+testNumber_2= float(100)
+print(testNumber_2)
+
+age= 10
+
+# concatenate = means combine
+print('Hi, I am '+ str(age) + ' years old.')
+print(f'Hi. Iam {age} yeard old.')
+
+# SyntaxError = an error where the computer 
+# doesn't understand the structure of your code. 
+
+datatype = "string"
+data
