3656b4e091e8d01b64d67d2bd7ca29cfa4673c0d
Ian <38480036+Iankimble@users.noreply.github.com>
2024-02-16 13:41:49 +0000

    Quiz 1 Feb 16

---
+++ Quiz1.py
@@ -0,0 +1,59 @@
+#1. What built-in function would you use if you wanted
+# to print out data to a user?
+
+#2. What built-in function would you use if you wanted 
+# a user to pass in data to a program?
+
+#3. Explain what complexity and abstraction are, then 
+# provide an example describing their relationship to one another.
+# You cannot use the following examples because they 
+# were shared in class; grocery/ electronics store, car, phone. 
+
+#4. What does it mean when you encounter a syntax error?
+
+#5. What is a variable? Provide an example of a variable in code.
+
+#6. Write a function in Python that when called will print out 
+# your name.
+
+#7. What is a function definition and a function call?
+
+#8. Describe what each symbol means?
+
+# = // single equal
+# == // double equal
+# != // exclaimation equal
+
+#9. What will the output for the following code block be?
+var1 = 1
+var2 = 2
+var3 = "3"
+
+print(var1 + var2 + var3)
+
+
+#10. What will the output for the following code block be?
+listOne = [20, 40, 60, 80]
+listTwo = [20, 40, 60, 80]
+
+print(listOne == listTwo)
+
+# 11. Create the following python lists and include the following
+# data types for each list:
+# a- create a python list of strings. The strings should be items you
+# can find in a book bag.
+# b- Create a python list of floats. the floats should represent the 
+# price of each item in a book bag from the previous list.
+
+# 12. What are the Three (3) naming formats we use for naming variables, files, and functions?
+
+# 13. Provide a coding scenario where you would you use a comparison operator in a program?
+
+# 14. Provide a coding scenario Where you would you use a logical operator in a program?
+
+# 15. Create a block of code where you would 
+# use a comparison operator with the follwing data:
+currentScore= 100
+highScore=2300
+
+# Bonus : What does the type() function do?
