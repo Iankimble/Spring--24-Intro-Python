9b40001473ae47ceacc3aefb1753f082664f7445
Ian <38480036+Iankimble@users.noreply.github.com>
2024-03-04 03:15:37 +0000

    Async Work Monday March 3rd, 2024

--- asyncWork_march3.py
+++ asyncWork_march3.py
@@ -1,19 +1,25 @@
 # Async Assignment March 3rd, 2024. 
+
 # Complete and solve the following prompts.
-# With each prompt, write down the clues and keywords you found.
-# This will count toward your grade. 
+# Make sure to write down the clues and keywords you found.
+# (Make sure to include your clues and keywords to recieve full credit)
 
+# Commit and sync your work before the end of class. 
+# This will count toward your class activity grade. 
 
-#1. Elevator Conditional Function Execercise.
+# Prompt #1- Elevator Conditional Function Execercise.
 
 # You have been hired by a architeture firm to assist 
 # with developing their elevator system. They have asked 
-# you to develop a function that will accept user's input. 
+# you to develop a function that will accept a user's input. 
+# your program should ask the user to enter a floor number. 
 # When an user enters a number they will be sent to a specific 
 # floor in the building. If the user enters a floor number that does not
 # exist, your program should inform the user that the floor they entered
-# does not exist and to enter and to try agin. The architects have
-# given you the following lists of floors for their building:
+# does not exist and to enter another number and to try agin.
+
+# The architects have given you the following 
+# lists of floors for their building:
 
 # M = 'lobby'
 # B = 'basement'
@@ -23,11 +29,14 @@
 # 3 = 'workspace'
 # 4 = 'living quarters'
 
+#___________________________________________________________
 
-# 2. You have been hired by an amusement park to develop a program
-# that allows users to access their roller coasters by entering the age,
-# height. Your client has very specific requirements for thier guests to
-# ride their roller coasters and have provided you with the following 
+# Prompt #2- Amusement Park Conditional Function
+# You have been hired by an amusement park to develop a function
+# that allows users to access their roller coasters by entering their age
+# and height. Your program should take the user's height and age as parameters.
+# Your client has very specific requirements for thier guests 
+# to ride their roller coasters and have provided you with the following 
 # conditions that they would like your program to check for. 
 
 # user must be atleast 5.2 or taller and atleast 14 years old or older 
