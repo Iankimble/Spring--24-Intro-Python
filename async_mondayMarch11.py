98bb1697e470b8744bd39ea5bf29cdd36f11a857
Ian <38480036+Iankimble@users.noreply.github.com>
2024-03-22 13:55:55 +0000

    Makeup day

---
+++ async_mondayMarch11.py
@@ -0,0 +1,38 @@
+# Intro to Python Async Work
+# Monday March 11th, 2024.
+
+# INSTRUCTIONS
+# Create a new python file called async_3_11.py
+# Answer the following questions. Once you've completed the questions, commit
+# and sync your work.
+# This activity must be completed by the end of class in order to recieve
+# a grade.
+
+# REMEMBER - TAKE YOUR TIME AND DO YOUR BEST! DO NOT GIVE UP! 
+
+# 1. Which Python datacasting function would
+# you use if you wanted to convert a string data type
+# into an integer data type? Please write your response
+# in a complete sentence. 
+
+
+# keywords
+# function
+# converts a string into an integer 
+# datacasting - converts data types into other data types
+
+# 2. Create a list called numbCol that contains three (3 ) colors and three (3) numbers.
+
+# 3. You have been hired by a University to create
+# a scholarship function. The client would like to provide 
+# students a scholarship to their school based on the following
+# conditions; 
+#- If the user has never gotten a loan before and,
+#- If the user has never been to college before.
+
+# The client would like you to use the logical NOT operator that will
+# compare the condtions and return false. If the result is false, the client
+# would also like the program to print the message "congrats! you've gotten the scholarship."
+# the client has given you the choice on how to enter data for your function.
+# you may enter data using input or pass in data into your function as parameters. 
+
---
+++ async_mondayMarch18.py
@@ -0,0 +1,29 @@
+import random
+
+# Async work Monday March 18th, 2024.
+
+#1. Create a function that will count the number of characters in a string
+# that is passed in by a user. The string value can be passed in either as 
+# a paramter or by using the input function.
+ 
+# You must be write down and explain how your program 
+# works in complete sentences in order to get full credit. 
+
+# 2. Use your notes, W3schools, and what we
+# have learned in class to develop
+# a simple rock, paper, scissors game. 
+# Your game should allow the user to enter a letter
+# that will represent the values rock, paper,
+# and scissors (ex. r = rock, p = paper, s= scissors).
+
+# EXTREMELY IMPORTANT NOTE
+# at the top of you page write import random
+# use the random.randrange(1,3) function to
+# develop your random value logic
+# for your rock, paper, scissor game. 
+
+# Please describe the steps you took, 
+# or if you weren't able to complete this activity,
+# the steps you would've taken to solve 
+# this problem in complete sentences. 
+# This must be completed in order to get full credit.  
