dd82d41ae469067ff8719566b4acc7b927a439ec
Ian <38480036+Iankimble@users.noreply.github.com>
2024-04-01 04:12:18 +0000

    async monday april 1

---
+++ async_mondayApril1.py
@@ -0,0 +1,21 @@
+# Async Activity - Monday April 1st, 2024.
+
+# Watch the following videos and answer the following questions. 
+# If necessary, research the questions to so that you are able to answer them better.
+
+# Video # 1 - Racial wealth gap
+# https://www.youtube.com/watch?v=Mqrhn8khGLM&t=1s
+
+# Video # 2 - Red lining in America 
+# https://www.youtube.com/watch?v=O5FBJyqfoLM
+
+# Search and find the latest (most recent) household income data 
+# for a black households compared to a white households and write down what you have found.
+# What is the wealth disparity between the two groups? How does this make you feel?
+
+# Research and define in your own words what red lining is?
+
+# Research and define in your own words what compound interest is?
+
+
+ 
