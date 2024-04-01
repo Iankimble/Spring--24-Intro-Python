0c0d5e21360a80fbc34a9c598c67c6c713d6b21c
Ian <38480036+Iankimble@users.noreply.github.com>
2024-04-01 04:15:40 +0000

    Async Monday April 1st 2024

--- async_mondayApril1.py
+++ async_mondayApril1.py
@@ -1,7 +1,10 @@
 # Async Activity - Monday April 1st, 2024.
 
 # Watch the following videos and answer the following questions. 
-# If necessary, research the questions to so that you are able to answer them better.
+# If necessary, research the questions so that you are able to answer them more effictively.
+
+# When you have completed this assignment, save your work as asyncApril1.py.
+# you must submit your work by 11:59 pm today (April 1, 2024) to recieve credit.  
 
 # Video # 1 - Racial wealth gap
 # https://www.youtube.com/watch?v=Mqrhn8khGLM&t=1s
@@ -9,13 +12,16 @@
 # Video # 2 - Red lining in America 
 # https://www.youtube.com/watch?v=O5FBJyqfoLM
 
-# Search and find the latest (most recent) household income data 
+
+# Questions
+
+#1. Search and find the latest (most recent) household income data 
 # for a black households compared to a white households and write down what you have found.
 # What is the wealth disparity between the two groups? How does this make you feel?
 
-# Research and define in your own words what red lining is?
+#2.  Research and define in your own words what red lining is?
 
-# Research and define in your own words what compound interest is?
+#3.  Research and define in your own words what compound interest is?
 
 
  
