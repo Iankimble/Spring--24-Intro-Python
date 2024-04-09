e078463d4333eeeb595a839de2a15fdeb72ebc83
Ian <38480036+Iankimble@users.noreply.github.com>
2024-04-09 13:58:32 +0000

    extra credit april 9

---
+++ extraCredit_April9.py
@@ -0,0 +1,37 @@
+# Prompt #1. 
+#Create a function that will count the number of characters in a string
+# that is passed in by a user. The string value can be passed in either as 
+# a paramter or by using the input function.
+
+# Prompt #2.
+# You have been hired by an amusement park to develop a function
+# that allows users to access their roller coasters by entering their age
+# and height. Your program should take the user's height and age as parameters.
+# Your client has very specific requirements for thier guests 
+# to ride their roller coasters and have provided you with the following 
+# conditions that they would like your program to check for. 
+
+# user must be atleast 5.2 or taller and atleast 14 years old or older 
+# in order to ride roller coaster 1. 
+
+# if the user is shorter than 5.2 but at least 14 years of age or older,
+# they can ride the roller coaster 2.
+
+# if the user is shorter than 5.2 and also younger than 14 years of age,
+# they can ride roller coaster 3. 
+
+# if the user enters information incorrectly, give them an error message
+# and tell them that they entered their information incorrectly. 
+
+# Prompt # 3.
+# You have been hired as an engineer to develop a checkout scanner system.
+# when a user puts in a name of an item and the price of the item it gets added to their cart.
+# your function to loop/ repeat and ask the user if they are done shopping or would like to continue
+# if the enter 'y' for yes, continue the loop and allow the user to put in another
+# item name and item price. If they enter 'n' for no, calculate the total price of items as
+# well as print the all the item names.
+
+# bonus for prompt # 3.
+# create a discount system where, when the user enters 'y' for being done shopping,
+# ask the user if they have a discount code. Depending on the code they enter they will
+# recieve 10%, 25%, or 50% off of their total purchase. 
