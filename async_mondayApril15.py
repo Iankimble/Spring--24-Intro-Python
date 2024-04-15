8584e56e52f5384521fd9d5af8207ac6f52efadc
Ian <38480036+Iankimble@users.noreply.github.com>
2024-04-15 01:39:17 +0000

    async work monday April 15

---
+++ async_mondayApril15.py
@@ -0,0 +1,25 @@
+# Complete the following questions. This assignment must be submitted
+# on Monday April 15th, 2024, by 11:59pm to recieve full credit. 
+
+#1. Look up and read about the Range keyword on W3 schools. 
+# Do the "try now" example provided on the article page and in your async document, 
+# in your own words, describe what the range() function does.  
+
+# Prompt # 1 
+# Create a python list of fruits. 
+# Your list of fruits should contain mainly apples, organges,
+# and a few other random fruits of your choice. 
+# Once you have your list, create a python function that 
+# uses a python For loop to go through the fruit list. 
+# If the loop runs into a fruit with the value of apple, send 
+# that value to a new list variable called apple list. If the loop encounter 
+# a fruit with the value of orange, it should move the item to 
+# a new list variable called orange list. Else, if the loop encounters
+# any other fruit, it should move that item to a list called unsorted fruits.
+
+# Note: In order to recieve the minimum amount of credit, 
+# write down any clues or keywords that you recognize in the prompt 
+# and describe how you would solve this problem if you get stuck. 
+
+
+
