a72f1d3dde9f4febb86a2c9e3a579ef404b933fd
Ian <38480036+Iankimble@users.noreply.github.com>
2024-04-16 13:04:18 +0000

    warmup April 16th

---
+++ warmup_April16.py
@@ -0,0 +1,21 @@
+# Warm up Tuesday April 16th, 2024.
+
+import random
+
+# 1. In your own words, explain the difference between an Python For Loop and a
+# Python While Loop.
+
+# 2. Create a loop that will iterate over a list of numbers. For evey number in your loop,
+# it should multiply that number by 3. 
+
+# 3. Use the following code snippet below to create a guessing game function. 
+# The code below will generate a random number for you. You must write a loop that will
+# ask the user to input their guess, if they guess incorrectly, the program will repeat 
+# itself and ask the user to guess again. The program should continue to ask them to
+# guess until they've gotten it correctly. Once they guess the correct number the program
+# will congratulate them and the loop will stop. 
+
+# generates a random number between 1 and 10
+randomNumber = random.randint(1, 10)
+
+print('Random number value is: {randomNumber}')
