3a2cfd397ced75a87885a1ff18217a7179017664
Ian <38480036+Iankimble@users.noreply.github.com>
2024-04-19 13:56:41 +0000

    activity april 19

--- pythonRevewActivity_April19.py
+++ pythonRevewActivity_April19.py
@@ -1,6 +1,5 @@
-#1A.  Visit the following web page and read the article on asking questions.
+#1A. Visit the web page link below and read the following article on asking questions:
 # https://www.betterup.com/blog/how-to-ask-good-questions
-
 
 # 1B. Based on what you've read, in your own words, describe what a good
 # question is.
@@ -8,22 +7,22 @@
 # 1C. In your opinion and in your own words, why do you think its
 # important to be able to ask good questions.
 
-#2A. Visit the following webpage and complete the learning style survey
+# 2A. Visit the link web page below and complete the following learning style survey:
 # http://www.educationplanner.org/students/self-assessments/learning-styles-quiz.shtml
 
-#2B. Based on your completed survey results, which learning style are you? List, three 
-# things from the report that you believe are true. 
+#2B. Based on your completed survey results, which learning style are you? List, three (3)
+# things from the survey results that you feel best describe your learning style. 
 
 #2B. Based on your completed survey results, do you feel as though the result are arcurate in
-# reflecting your learning style, if yes, explain why, if no, explain why. Please write your
-# response in complete sentences. 
+# reflecting your learning style? if yes, explain why, if no, explain why. Please write your
+# response in atleast three (3) complete sentences. 
 
-# 3A. Visit the following webpage and read the article on how to study
+# 3A. Visit the  webp age link below and read the article on how to study:
 # https://summer.harvard.edu/blog/top-10-study-tips-to-study-like-a-harvard-student/
 
 # 3B. Which of the following tips do you feel you need to work on when studying? Explain
-# why in complete sentences.
+# why in at least three (3) complete sentences.
 
-# 3C. Based on your learning style results, and the arcticle you just read on study tips
+# 3C. Based on your learning style results, and the arcticle you just read on study tips,
 # think about how you can apply the study tips to your learning style. Then, in complete sentences
 # write down four (4) steps you could take to effectively study that is specific you you. 
