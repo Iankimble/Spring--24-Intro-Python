3f70b6e03a0050ad5b87a804f892260f36d801b9
Ian <38480036+Iankimble@users.noreply.github.com>
2024-04-22 11:40:49 +0000

    async work monday april 22

---
+++ async_mondayApirl22.py
@@ -0,0 +1,29 @@
+# Read the following prompt and answer the 3 questions
+
+# Prompt
+
+# You have been tasked as an engineer to develop a program that controls the lights in a small home.
+# When the users uses the lighting control app that you are developing, they will have the ability to 
+# turn lights on and off in the entire house. They can turn lights on and off based on the room in the home 
+# individually or all the lights in the home at the same time. 
+# there are 3 bedrooms, 1 kitchen, 2 bathrooms, a living room, a dining room, and a basement that all
+# have lights and can all be manipulated with by using this app. 
+
+# Questions
+
+# 1. Based on the prompt you just read, what is the main goal(s) of the app that you are creating?
+# What is the primary objective of your app? Please write your response using complete a complete sentence.
+
+# 2. What type of research would you need to do to help you develop this app? And are there 
+# any questions that you can think of that could give you more context/ information
+# that can help you develop this program that wasn't included ? 
+# Please write your response in at least 4 complete sentences. 
+
+# 3. Breakdown step- by- step how you would create this program in Python? Your steps do not have
+# to be written in code and it does not have to use python keywords- rather your steps 
+# should be explained in a way where an engineer can take the information and understand
+# what they need to do.  Please be sure to write your steps in complete sentences. 
+
+
+
+
