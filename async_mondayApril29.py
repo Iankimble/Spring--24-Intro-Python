30f21a7e03babaa629dd23778050fc5d2a9afcce
Ian <38480036+Iankimble@users.noreply.github.com>
2024-04-29 12:03:34 +0000

    async Monday April 29

---
+++ async_mondayApril29.py
@@ -0,0 +1,7 @@
+# Use your time wisely and constrcutively to review class notes
+# and activities in preparation for tomorrows quiz.
+
+# Provided below is a website that has practice questions that you 
+# can do to sharpen your skills
+
+# https://pynative.com/python-exercises-with-solutions/
